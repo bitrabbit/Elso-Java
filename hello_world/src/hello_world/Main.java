70c33244fc13741506eeefe4352ff2311b5c5bf6
Bence Pusztai <pusztai.bence@hotmail.com>
2018-10-06 10:58:25 +0200

    init

---
+++ hello_world/src/hello_world/Main.java
@@ -0,0 +1,33 @@
+
+package hello_world;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		/*int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int c = Integer.parseInt(args[2]);
+
+		int terfogat = a * b * c;
+		int felszin = 2 * (a * b + a * c + c * b);
+
+		System.out.println("Térfogat: " + terfogat);
+		System.out.println("Felszín: " + felszin);*/
+		
+		//------------------------------------------
+		
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		
+		int osszeg = a+b;
+		int szorzat = a*b;
+		double hanyados = a/b;
+		int kulonbseg = a-b;
+		
+		System.out.println("Összeg: " + osszeg);
+		System.out.println("Szorzat: " + szorzat);
+		System.out.println("Hányados: " + hanyados);
+		System.out.println("Különbség: " + kulonbseg);
+	}
+}
