9a204ed78569313e67583c06e08804bd62e53101
Bence Pusztai <pusztai.bence@hotmail.com>
2018-10-12 15:55:06 +0200

    Adatbekérés

---
+++ szoft1012/src/szoft1012/Main.java
@@ -0,0 +1,40 @@
+package szoft1012;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	Scanner beolvas = new Scanner(System.in);
+//	System.out.println("Kérek egy szót: ");
+//	String beolvasottAdat = beolvas.nextLine(); //<- több sor beolvasása a next csak egy
+//	System.out.println(beolvasottAdat);
+//	beolvas.close();
+	
+//	try {
+//		System.out.println("Kérek egy számot: ");
+//		int szam = beolvas.nextInt();
+//		double eredmeny =(double) 1/szam;
+//		System.out.println("A szám reciproka: ");
+//		System.out.println(eredmeny);
+//	} catch (InputMismatchException e) {
+//		System.out.println("Hibás számformátum! \n Számot kérek!");
+//	}
+//	beolvas.close();
+	
+	try {
+		System.out.println("Kérek egy karaktert: ");
+		int karakter = beolvas.next().charAt(0);
+		System.out.println("A karakter: ");
+		System.out.println(karakter);
+	} catch (InputMismatchException e) {
+		System.out.println(e);
+	}
+	beolvas.close();
+	
+	}
+
+}
